e855b056eeeac6ba9ec66746a5ad158bd5bc5ef7
Aleana-Beat0 <adbeato19@gmail.com>
2025-02-26 13:50:34 +0800

    withjson

--- client.py
+++ client.py
@@ -1,28 +1,27 @@
-#Built in libary of socket
+# Built-in library of socket
 import socket
+import json
+
+address = "127.0.0.1"  # IP address
+port = 8000             # Port number
+json_file_path = 'input.json'  # Path to the JSON file
+
+# Function to send the JSON file content to the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+    client.connect((address, port))
+
+    # Read the JSON data from the file
+    with open(json_file_path, 'r') as json_file:
+        json_data = json_file.read()
+
+    # Send the JSON data to the server
+    client.sendall(json_data.encode('utf-8'))  # Send the JSON content as bytes
+
+    # Receive the server's response (optional)
+    response = client.recv(1024).decode()
+    print(f"Received from server: {response}")
+    
+print("Connection closed")
 
 
-address = "127.0.0.1" #IP address
-port = 8000 #port number
-msg = "Hello, server!"  # Message 
-
-#Function
-def run_client():
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-        client.connect((address, port))
-
-        while True:
-            client.send(msg.encode()[:1024])  
-
-            response = client.recv(1024).decode()
-            if response.lower() == "closed":
-                break
-
-            print(f"Received: {response}")
-            break
-
-    print("Connection closed")
-
-#Function Declaration
-run_client()
--- server.py
+++ server.py
@@ -1,23 +1,32 @@
 import socket
+import csv
+import json
 
 address = "127.0.0.1"
 port = 8000
 
-def run_server():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-        server.bind((address, port))
-        server.listen(1)
-        print("Listening on 127.0.0.1:8000")
 
-        client_socket, client_address = server.accept()
-        with client_socket:
-            print(f"Connection from {client_address[0]}:{client_address[1]}")
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+    server.bind((address, port))
+    server.listen(1)
+    print("Listening on 127.0.0.1:8000")
 
-            request = client_socket.recv(1024).decode() 
-            print(f"Received: {request}")
+    client_socket, client_address = server.accept()
+    
+    with client_socket:
+        print(f"Connection from {client_address[0]}:{client_address[1]}")
 
-            client_socket.send("closed".encode())
+        data = client_socket.recv(1024).decode() 
+        
+        jf = json.loads(data)
 
-    print("Connection closed")
+        with open("trial.csv", 'a') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=data[0].keys())
+            writer.writerows(jf)
+            print("Successful Data")
 
-run_server()
+
+        client_socket.send("closed".encode())
+
+print("Connection closed")
+
